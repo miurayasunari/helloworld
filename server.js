dce7a542a7ac7e48f5c0e7fe5333e15534ff2d8b
miurayasunari <maeyama202@gmai.com>
2018-11-24 16:13:30 +0900

    change massage

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var server = http.createServer(function(req, res) {
 	console.log('Connected');
 	res.statudCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World!\n');
+	res.end('Hello World! by yasunari\n');
 });
 
 server.listen(port, function() {
