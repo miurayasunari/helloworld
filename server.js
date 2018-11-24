419e945720d436c315521fc709882fa52ddfa787
miurayasunari <maeyama202@gmai.com>
2018-11-24 15:25:20 +0900

    create server

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+var port = 3000;
+var server = http.createServer(function(req, res) {
+	console.log('Connected');
+	res.statudCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World!\n');
+});
+
+server.listen(port, function() {
+	console.log('server running');
+});
